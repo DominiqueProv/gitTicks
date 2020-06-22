3d6eed522b08e65ad2ecfcc4dfbba75e1a8e29df
Dominiqueprov <dominiquepro@hotmail.com>
2020-06-22 15:44:11 -0400

    update

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Test');
+  console.log('Test update');
   //check if the key exist, if not will throw an error
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
