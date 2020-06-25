ff0bdc37c2f4ca35a07b545cf59e7cd34c22e93b
Dominiqueprov <dominiquepro@hotmail.com>
2020-06-25 15:52:29 -0400

    test

--- client/.next/static/runtime/webpack.js
+++ client/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1112de62bbfa12d8fd80";
+/******/ 	var hotCurrentHash = "c109c38c4315b01de9ab";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
